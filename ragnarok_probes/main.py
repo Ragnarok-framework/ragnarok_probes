876de0075bde44edae16d303ec6002571c185043
LuboNenov <liubomir.n.2019@feg.bg>
2021-11-20 16:16:30 +0200

    Added optimisations and vendors for ARP spoofer

--- ragnarok_probes/main.py
+++ ragnarok_probes/main.py
@@ -1,19 +1,18 @@
 from netscan import Netscan
 import argparse
 
-class Main():
+class Main:
 
-    def main():
+    def main(self):
         parser = argparse.ArgumentParser()
         parser.add_argument('-t', '--target', dest='target', help='Target IP Address/Adresses')
         options = parser.parse_args()
         if not options.target:
             parser.error("[-] Please specify an IP Address or Addresses, use --help for more info.")
         return options
-    options = main()
-    scanned_output = scan(options.target)
-    display_result(scanned_output)
 
 
 if __name__ == '__main__':
-   Main().main()
+   option = Main().main()
+   scanned_output = Netscan().scan(option.target)
+   Netscan().display_result(scanned_output)
--- ragnarok_probes/netscan.py
+++ ragnarok_probes/netscan.py
@@ -1,9 +1,9 @@
-import scapy
-
-class Netscan():
+from scapy import all as scapy
+import requests
+class Netscan:
     """ Scans a range of IPs """
     def scan(self, ip):
-        arp_req_frame = scapy.ARP(pdst = self.ip)
+        arp_req_frame = scapy.ARP(pdst = ip)
 
         broadcast_ether_frame = scapy.Ether(dst = "ff:ff:ff:ff:ff:ff")
 
@@ -12,12 +12,17 @@ class Netscan():
         answered_list = scapy.srp(broadcast_ether_arp_req_frame, timeout = 1, verbose = False)[0]
         result = []
         for i in range(0,len(answered_list)):
-            client_dict = {"ip" : answered_list[i][1].psrc, "mac" : answered_list[i][1].hwsrc}
+            client_dict = {"ip" : answered_list[i][1].psrc, "mac" : answered_list[i][1].hwsrc, "vendor" : mac_vendor(answered_list[i][1].hwsrc)}
             result.append(client_dict)
 
         return result
 
+    def mac_vendor(mac):
+        url = "https://api.macvendors.com/"
+        response = requests.get(url+mac)
+        return response.content.decode()
+
     def display_result(self, result):
         print("-----------------------------------\nIP Address\tMAC Address\n-----------------------------------")
         for i in result:
-            print("{}\t{}".format(i["ip"], i["mac"]))
+            print("{}\t{},{}".format(i["ip"], i["mac"], i["vendor"]))
